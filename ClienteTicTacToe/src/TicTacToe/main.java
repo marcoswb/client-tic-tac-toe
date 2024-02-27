77dc1b81b9c0fdda95d1dc31657a2ad584c983e1
marcos_wb <88458130+MarcosWarmlingTR@users.noreply.github.com>
2024-02-26 21:17:31 -0300

    criada telas de login e registro

--- ClienteTicTacToe/src/TicTacToe/main.java
+++ ClienteTicTacToe/src/TicTacToe/main.java
@@ -1,13 +1,16 @@
 package TicTacToe;
 
-import TicTacToe.view.Board;
+import TicTacToe.view.LoginScreen;
 
 public class main {
 
     public static void main(String[] args) {
-        Board main_screen = new Board();
-        main_screen.SetPlayerCharacter("O");
-        main_screen.show();
+//        Board main_screen = new Board();
+//        main_screen.SetPlayerCharacter("O");
+//        main_screen.show();
+        
+        LoginScreen login_screen = new LoginScreen();
+        login_screen.show();
     }
     
 }
