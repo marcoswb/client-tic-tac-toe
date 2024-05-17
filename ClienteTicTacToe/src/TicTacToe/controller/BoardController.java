3473d3ab6d9cd8d0c5350d16d1bc594724a0544a
marcos_wb <88458130+MarcosWarmlingTR@users.noreply.github.com>
2024-05-16 21:12:29 -0300

    adicionada comunição basica com servidor socket

--- ClienteTicTacToe/src/TicTacToe/controller/BoardController.java
+++ ClienteTicTacToe/src/TicTacToe/controller/BoardController.java
@@ -3,6 +3,8 @@ package TicTacToe.controller;
 import javax.swing.JLabel;
 import raven.swing.spinner.SpinnerProgress;
 import static javax.swing.WindowConstants.HIDE_ON_CLOSE;
+import java.io.*;
+import java.net.*;
 import TicTacToe.view.Board;
 
 
@@ -11,6 +13,11 @@ public class BoardController implements Runnable{
     private String nickname;
     private JLabel jLabelProgress;
     private SpinnerProgress spinnerProgressLoad;
+    private final String host = "127.0.0.1";
+    private final int port = 8000;
+    private final byte[] data = new byte[1024];
+    private Socket socket;
+    private String response;
     
     public void startGame(JLabel label, SpinnerProgress spinner){
         
@@ -27,26 +34,82 @@ public class BoardController implements Runnable{
     @Override
     public void run(){
         try{
-            API api = new API();
-            String nickname_oponent = api.GetFreeUser(this.getNickname());
+//            API api = new API();
+//            String nickname_oponent = api.GetFreeUser(this.getNickname());
             
-            Board board = new Board();
-            
-            board.setPlayer_01(this.getNickname());
-            board.setPlayer_02(nickname_oponent);
-            
-            board.setDefaultCloseOperation(HIDE_ON_CLOSE);
-            board.StartGame();
+//            board.setPlayer_01(this.getNickname());
+//            board.setPlayer_02(nickname_oponent);
+        
+//            Board board = new Board();
+//            
+//            board.setDefaultCloseOperation(HIDE_ON_CLOSE);
+//            board.StartGame();
+//
+//            this.jLabelProgress.setText("");
+//            this.spinnerProgressLoad.setValue(0);
+//            this.spinnerProgressLoad.setIndeterminate(false);
 
-            this.jLabelProgress.setText("");
-            this.spinnerProgressLoad.setValue(0);
-            this.spinnerProgressLoad.setIndeterminate(false);
+            
+            try{
+                socket = new Socket(host, port);
+                
+                this.sendMessage("entrar");
+                
+                String firstResponse = this.awaitMessage();
+                System.out.println("Server first response: " + firstResponse);
+
+                OUTER:
+                while (true) {
+                    switch (firstResponse) {
+                        case "await":
+                            response = this.awaitMessage();
+                            System.out.println("Server response: " + response);
+                            this.sendMessage("entrar");
+                            break;
+                        case "play":
+                            this.sendMessage("entrar");
+                            response = this.awaitMessage();
+                            System.out.println("Server response: " + response);
+                            break;
+                        default:
+                            break OUTER;
+                    }
+                }
+                
+                System.out.println("SAIUUUUUUUUU");
+            } catch (UnknownHostException ex) {
+                System.out.println("Server not found: " + ex.getMessage());
+            } catch (IOException ex) {
+                System.out.println("I/O error: " + ex.getMessage());
+            }
             
         } catch (Exception ex) {
             System.out.println("MARCOSSSS " + ex);
         }
     }
-
+    
+    public void sendMessage(String message) throws IOException{
+        OutputStream output = socket.getOutputStream();
+        output.write(message.getBytes());
+        output.flush();
+    }
+    
+    public String awaitMessage() throws IOException{
+        InputStream input = socket.getInputStream();
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        
+        int nRead;
+        while ((nRead = input.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+            if (input.available() == 0) {
+                break;
+            }
+        }
+        String response = buffer.toString("UTF-8");
+        
+        return response;
+    }
+    
     public void setjLabelProgress(JLabel jLabelProgress) {
         this.jLabelProgress = jLabelProgress;
     }
