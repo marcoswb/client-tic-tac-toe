8de0aea85fff3192d38532fdf2caf11b2d19fd49
marcoswb <marcos_wb@outlook.com>
2025-01-09 13:29:56 -0300

    não travar thread  ao gerar aviso joptionpane

--- ClienteTicTacToe/src/main/java/view/ErrorDialog.java
+++ ClienteTicTacToe/src/main/java/view/ErrorDialog.java
@@ -5,10 +5,20 @@ import javax.swing.JOptionPane;
 
 public class ErrorDialog {
 
+    public String message;
+    
     public static void main(String args[]) {
     }
 
     public void SetMessage(String message) {
+        ErrorDialog controller = new ErrorDialog();
+        controller.message = message;
+        
+        Thread t = new Thread(controller::showMessage);
+        t.start();
+    }
+    
+    private void showMessage(){
         JOptionPane.showMessageDialog(new JFrame(), message, "Erro", JOptionPane.ERROR_MESSAGE);
     }
 }
--- ClienteTicTacToe/src/main/java/view/InfoDialog.java
+++ ClienteTicTacToe/src/main/java/view/InfoDialog.java
@@ -4,11 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class InfoDialog {
+    
+    public String message;
 
     public static void main(String args[]) {
     }
 
     public void SetMessage(String message) {
+        InfoDialog controller = new InfoDialog();
+        controller.message = message;
+        
+        Thread t = new Thread(controller::showMessage);
+        t.start();
+    }
+    
+    private void showMessage(){
         JOptionPane.showMessageDialog(new JFrame(), message, "Info", JOptionPane.INFORMATION_MESSAGE);
     }
 }
